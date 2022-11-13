ff7bdf64bef29bb0d54267ee06abdad0711fdd38
ShuaibuMaryam <shuaibumaryam113@gmail.com>
2022-11-13 09:14:40 +0100

    first commit

---
+++ src/Components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div>
+       <div className='hero-text'></div>
+       <div className='hero-images'></div> 
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/Components/Navbar.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import logo from '../Assets/home/logo.png'
+
+function Navbar() {
+
+    const [isNavExpanded, setIsNavExpanded] = useState(false)
+
+    const toggleIsNavExpanded = () => {
+        setIsNavExpanded(!isNavExpanded)
+    }
+
+  return (
+    <div>
+        <nav className='navigation'>
+            <div className='navbar-brand'>
+                <a href='#'><img src={logo}/></a>
+            </div>
+            
+            <div className={isNavExpanded ? "nav-menu expanded" : "nav-menu"}>
+                
+                <ul className="nav-links">
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>Place to stay</a></li>
+                    <li><a href='#'>NFTs</a></li>
+                    <li><a href='#'>Community</a></li>
+                </ul>
+
+                <div className="nav-btn-wrapper">
+                    <button className='nav-btn'>Connect wallet</button>
+                </div>
+
+            </div>
+
+
+            <div className="hamburger" onClick={toggleIsNavExpanded}>
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </div>
+        </nav>
+
+    </div>
+  )
+}
+
+export default Navbar
