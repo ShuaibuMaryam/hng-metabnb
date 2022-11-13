24a6c9dcb8863751484af3bde134d008939ca16b
ShuaibuMaryam <shuaibumaryam113@gmail.com>
2022-11-13 11:37:28 +0100

    feat: hero component

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import heroImage from '../Assets/home/hero-img.png'
+import '../Styles/hero.css'
 function Hero() {
   return (
-    <div>
-       <div className='hero-text'></div>
-       <div className='hero-images'></div> 
+    <div className='hero'>
+       <div className='hero-text'>
+            <h1 className='hero-text-heading'>Rent a <span>Place</span> away from <span>Home</span> in the <span>Metaverse</span></h1>
+            <p className='hero-text-desc'>we provide you access to luxury and affordable houses in the metaverse, 
+                get a chance to turn your imagination to reality at your comfort zone
+            </p>
+            <div className='hero-text-search'>
+                <input type="text" placeholder='Search for location' className='input-text'/>
+                <input type="submit" value="search" className='input-search'/>
+            </div>
+       </div>
+       <div className='hero-images'>
+            <img src={heroImage}/>
+        </div> 
     </div>
   )
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Hero from '../Components/Hero'
 import Navbar from '../Components/Navbar'
 
 function Home() {
   return (
     <div>
         <Navbar />
+
+        <Hero />
     </div>
   )
 }
